2b5c54b2bf0c26dd55d23223745c6e7d1267d250
Daniel Mouris <dgmouris@gmail.com>
2026-09-04 09:38:42 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/vehiclefleet_backend/urls.py
+++ 1_backend_review_start/vehiclefleet_backend/vehiclefleet_backend/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+
+# we're going to add the mappings
+# of our custom app as an api,
+# we need include,
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/", include("fleet.urls")),
 ]
