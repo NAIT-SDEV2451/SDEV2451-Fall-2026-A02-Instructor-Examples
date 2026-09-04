d9e1c30c38ae2c7e11f931c89c9fb1bebce004e2
Daniel Mouris <dgmouris@gmail.com>
2026-09-04 09:45:23 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/fleet/models.py
+++ 1_backend_review_start/vehiclefleet_backend/fleet/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+
+# let's make a vehicle model
+class Vehicle(models.Model):
+    # remember below is the cols representation
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveBigIntegerField()
+    licence_plate = models.CharField(max_length=100, unique=True)
+
+    # you want to be able to know what's in the model
+    # when you're reading a debugging.
+    def __str__(self):
+        return f" {self.make} {self.model} {self.year} ({self.licence_plate})"
