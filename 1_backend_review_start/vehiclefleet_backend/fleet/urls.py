f2a1e4b5be4084609d5d9f45cd7374064797a521
Daniel Mouris <dgmouris@gmail.com>
2026-09-09 09:19:17 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/fleet/urls.py
+++ 1_backend_review_start/vehiclefleet_backend/fleet/urls.py
@@ -1,1 +1,24 @@
-urlpatterns = []
+# let's hook up out views and viewsets to urls.
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+# we need to import all of our views
+from fleet.views import (
+    FleetStatsView,
+    DriverViewSet,
+    VehicleViewSet,
+    TripViewSet,
+)
+
+# let's create a router for the viewsets
+router = DefaultRouter()
+router.register("vehicles", VehicleViewSet, basename="vehicle")
+router.register("drivers", DriverViewSet, basename="driver")
+router.register("trips", TripViewSet, basename="trip")
+
+urlpatterns = [
+    # for an api view you need to specify each path for each endpoint.
+    path("stats/", FleetStatsView.as_view(), name="fleet-stats"),
+    # we're going to include all of the router urls.
+    path("", include(router.urls)),
+]
