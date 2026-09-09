07fb1ffc6c607db8af108913c0ec8f43328f093e
Daniel Mouris <dgmouris@gmail.com>
2026-09-09 08:23:00 -0600

    class progress pushed

--- 1_backend_review_start/vehiclefleet_backend/fleet/models.py
+++ 1_backend_review_start/vehiclefleet_backend/fleet/models.py
@@ -25,3 +25,27 @@ class Driver(models.Model):
     # we're going to add the str
     def __str__(self):
         return f"{self.name}, ({self.license_number})"
+
+
+class Trip(models.Model):
+    # we're going to link to both tables.
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.CASCADE,  # if a vehicle is deleted it deletes the trips.
+        related_name="trips",  # how to name trips on a vehicle
+        # you need to make a decision if it's blank/nullable
+    )
+    driver = models.ForeignKey(
+        Driver,
+        on_delete=models.CASCADE,  # if a driver is deleted it deletes the trips.
+        related_name="trips",  # how to name trips on a driver
+        # you need to make a decision if it's blank/nullable
+    )
+    # in the future we could make these lat longs
+    start_location = models.CharField(max_length=255)
+    end_location = models.CharField(max_length=255)
+    # start time and end time.
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField(null=True, blank=True)
+    # distance
+    distance = models.DecimalField(max_digits=8, decimal_places=2, null=True)
