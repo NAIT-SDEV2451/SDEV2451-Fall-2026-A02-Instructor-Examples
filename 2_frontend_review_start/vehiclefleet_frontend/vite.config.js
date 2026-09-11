19c8f459f66f8c6591438d32e0821882aae59c2c
Daniel Mouris <dgmouris@gmail.com>
2026-09-11 08:30:27 -0600

    class progress pushed

--- 2_frontend_review_start/vehiclefleet_frontend/vite.config.js
+++ 2_frontend_review_start/vehiclefleet_frontend/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// make tailwind available to our vite project.
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
-  plugins: [react()],
+  // include the plugin below.
+  plugins: [react(), tailwindcss()],
 })
