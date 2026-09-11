933e800a5260763a24f36d82c9defdd29c8c29bf
Daniel Mouris <dgmouris@gmail.com>
2026-09-11 09:35:05 -0600

    class progress pushed

---
+++ 2_frontend_review_start/vehiclefleet_frontend/src/components/TripForm.jsx
@@ -0,0 +1,10 @@
+// the form that creates new trips
+export default function TripForm({
+  // normal props where we're passing in a list.
+  vehicles,
+  drivers,
+  // you can pass callback functions to your components.
+  onSubmit
+}) {
+
+}
