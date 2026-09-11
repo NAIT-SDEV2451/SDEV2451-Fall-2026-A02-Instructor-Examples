a262369b1110e4c29c9e970b17ac639eb9096614
Daniel Mouris <dgmouris@gmail.com>
2026-09-11 09:37:44 -0600

    class progress pushed

--- 2_frontend_review_start/vehiclefleet_frontend/src/components/TripForm.jsx
+++ 2_frontend_review_start/vehiclefleet_frontend/src/components/TripForm.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 // the form that creates new trips
 export default function TripForm({
   // normal props where we're passing in a list.
@@ -6,5 +8,11 @@ export default function TripForm({
   // you can pass callback functions to your components.
   onSubmit
 }) {
+  // some form state.
+
+  // create some handler which will update the form state.
+
+  // we're going have some handle submit function
+  // which will call our onSubmit callback and clear state.
 
 }
