844097ce21381e1dc30dad8e083eeaf1489e72e6
Daniel Mouris <dgmouris@gmail.com>
2026-09-14 08:35:28 -0600

    class progress pushed

--- 2_frontend_review_start/vehiclefleet_frontend/src/App.jsx
+++ 2_frontend_review_start/vehiclefleet_frontend/src/App.jsx
@@ -1,12 +1,27 @@
 // this is going to be the "root" of our components in our application.
 // we're going to use react router to navigate to our pages here.
 // docs here: https://reactrouter.com/home
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <h1>Fleet Manager</h1>
+    <div className="min-h-screen bg-base-200">
+      <nav className="navbar bg-base-100 shadow px-6">
+        <div className="navbar-start">
+          <span className="text-lg font-bold">Fleet Manager</span>
+        </div>
+        <div className="navbar-end gap-2">
+
+        </div>
+      </nav>
+
+      <main className="p-6 max-w-6xl mx-auto">
+      </main>
     </div>
   )
 }
