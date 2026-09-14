20a1f882a103c9e304fe405421fd14fec5d2eb4b
Daniel Mouris <dgmouris@gmail.com>
2026-09-14 08:32:02 -0600

    class progress pushed

--- 2_frontend_review_start/vehiclefleet_frontend/src/App.jsx
+++ 2_frontend_review_start/vehiclefleet_frontend/src/App.jsx
@@ -1,3 +1,8 @@
+// this is going to be the "root" of our components in our application.
+// we're going to use react router to navigate to our pages here.
+// docs here: https://reactrouter.com/home
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+
 function App() {
   return (
     <div>
