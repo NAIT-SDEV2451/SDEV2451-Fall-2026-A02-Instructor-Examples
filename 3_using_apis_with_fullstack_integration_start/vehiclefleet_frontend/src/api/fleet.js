22f908a09b3e940b386bbc78b10cf014dd6e47e8
Daniel Mouris <dgmouris@gmail.com>
2026-09-16 09:19:21 -0600

    class progress pushed

---
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/api/fleet.js
@@ -0,0 +1,3 @@
+// the base url is different in production than your local server
+// most times this an environment variable and this points to our backend.
+const BASE_URL = `http://localhost:8000/api/v1`
