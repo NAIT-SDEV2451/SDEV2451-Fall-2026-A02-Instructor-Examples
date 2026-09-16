ee2e2a795b1a6b667968ac22c5524a87a35be428
Daniel Mouris <dgmouris@gmail.com>
2026-09-16 09:48:41 -0600

    class progress pushed

--- 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/VehiclesAndDriversPage.jsx
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/VehiclesAndDriversPage.jsx
@@ -2,12 +2,29 @@ import VehicleList from '../components/VehicleList'
 import DriverList from '../components/DriverList'
 import { VEHICLES, DRIVERS } from '../mockData'
 
+// we're going to use the hook
+import { useVehicles } from '../hooks/useVehicles'
+
 function VehiclesAndDriversPage() {
+  // we're going to use our new hook to get that data
+  // fetches when we call it immediately.
+  const {
+    vehicles, // data from the server.
+    isLoading,
+    isError,
+    error
+  } = useVehicles()
+
   return (
     <div className="flex flex-col gap-8">
       <section>
         <h2 className="text-xl font-semibold mb-3">Vehicles</h2>
-        <VehicleList vehicles={VEHICLES} />
+        {/* instead of mock data we're going to use our data here */}
+        { isLoading
+          ? <span className='loading loading-spinner loading-md'></span>
+          : <VehicleList vehicles={vehicles} />
+        }
+
       </section>
 
       <section>
