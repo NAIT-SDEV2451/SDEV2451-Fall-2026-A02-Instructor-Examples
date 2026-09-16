04b5b4ac9104e16b1046f8d1379185e204449320
Daniel Mouris <dgmouris@gmail.com>
2026-09-16 09:25:00 -0600

    class progress pushed

--- 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/api/fleet.js
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/api/fleet.js
@@ -1,3 +1,18 @@
 // the base url is different in production than your local server
 // most times this an environment variable and this points to our backend.
 const BASE_URL = `http://localhost:8000/api/v1`
+// note pay attention to the slashes in the url!
+
+// we're going to make the fetch request to the backend
+export async function fetchVehicles() {
+  // export makes it available to other files.
+  // make the request to the backend
+  const response = await fetch(`${BASE_URL}/vehicles/`)
+  // the above errors for 500s statuses but not 400s so let's throw an error if
+  // it is a 400
+  if (!response.ok) {
+    throw new Error("Failed to fetch vehicles")
+  }
+  // if it's here it should be a good request.
+  return response.json()
+}
