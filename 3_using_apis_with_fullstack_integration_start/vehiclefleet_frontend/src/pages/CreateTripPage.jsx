3bbc0d636c81d8d56ee3e6c6163b48f3ac938674
Daniel Mouris <dgmouris@gmail.com>
2026-09-18 09:22:49 -0600

    class progress pushed

--- 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/CreateTripPage.jsx
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/CreateTripPage.jsx
@@ -2,8 +2,22 @@ import { useNavigate } from 'react-router-dom'
 import TripForm from '../components/TripForm'
 import { VEHICLES, DRIVERS } from '../mockData'
 
+import { useCreateTrip } from '../hooks/useTrips'
+import { useVehicles } from '../hooks/useVehicles'
+import { useDrivers } from '../hooks/useDrivers'
+
 function CreateTripPage() {
   const navigate = useNavigate()
+
+  const {
+    mutate: createTripMutation,
+    // mutate/createTripMutation is a function
+    // that will execute the mutation
+    isPending // like loading
+  } = useCreateTrip()
+  // the data.
+  const { vehicles } = useVehicles()
+  const { drivers } = useDrivers()
 
   function handleSubmit(formData) {
     // In a real app: POST to /api/v1/trips/ then navigate
@@ -14,7 +28,10 @@ function CreateTripPage() {
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4">Create a New Trip</h2>
-      <TripForm vehicles={VEHICLES} drivers={DRIVERS} onSubmit={handleSubmit} />
+      <TripForm
+        vehicles={vehicles}
+        drivers={drivers}
+        onSubmit={handleSubmit} />
     </div>
   )
 }
