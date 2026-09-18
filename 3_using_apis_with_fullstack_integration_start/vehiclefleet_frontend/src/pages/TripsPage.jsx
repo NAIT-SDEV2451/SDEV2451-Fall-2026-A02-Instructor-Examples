d3a903ed69c4dfcf2a57f0f12b9c4c3796402da2
Daniel Mouris <dgmouris@gmail.com>
2026-09-18 09:14:40 -0600

    class progress pushed

--- 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/TripsPage.jsx
+++ 3_using_apis_with_fullstack_integration_start/vehiclefleet_frontend/src/pages/TripsPage.jsx
@@ -14,6 +14,21 @@ function TripsPage() {
     error
   } = useTrips()
 
+  // we're going to put a few guards
+  // one for loading
+  if (isLoading) {
+    return <div>
+      <h2 className="text-xl font-semibold mb-3">
+        Trips Loading...
+      </h2>
+      <span className='loading loading-spinner loading-md'></span>
+    </div>
+  }
+
+  // one for error state.
+
+
+
   return (
     <div>
       <h2 className="text-xl font-semibold mb-3">Trips</h2>
