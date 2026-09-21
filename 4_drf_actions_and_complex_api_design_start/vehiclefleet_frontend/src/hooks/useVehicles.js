434ee07fb3bbb2413eb50c064642cd9099fe7645
Daniel Mouris <dgmouris@gmail.com>
2026-09-21 09:27:09 -0600

    class progress pushed

--- 4_drf_actions_and_complex_api_design_start/vehiclefleet_frontend/src/hooks/useVehicles.js
+++ 4_drf_actions_and_complex_api_design_start/vehiclefleet_frontend/src/hooks/useVehicles.js
@@ -1,16 +1,24 @@
-import { useQuery } from '@tanstack/react-query'
+import {
+  useQuery,
+  keepPreviousData,
+} from '@tanstack/react-query'
 import { fetchVehicles } from '../api/fleet'
 
 // we need value to of search to be passed to the hook
 export function useVehicles(search = "") {
   const { data: vehicles = [], isLoading, isError, error } = useQuery({
-    queryKey: ['vehicles'],
+    // expand key so it includes search
+    queryKey: ['vehicles', search],
     // we need to pass it to the queryfn
     queryFn: () => fetchVehicles({
       // left is the search key on fetchVehicles
       // right is the search we'll pass into our hook.
       search: search
     }),
+    // below is just so that it doesn't blink.
+    placeholderData: keepPreviousData,
+    // while it's loading the new request it's keeping
+    // the old data.
   })
   return { vehicles, isLoading, isError, error }
 }
