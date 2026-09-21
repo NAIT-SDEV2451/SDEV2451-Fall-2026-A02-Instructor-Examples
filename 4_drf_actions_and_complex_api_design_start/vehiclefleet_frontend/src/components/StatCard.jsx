2af9c26fc2c7a105e51708217822ed427c9f3639
Daniel Mouris <dgmouris@gmail.com>
2026-09-21 09:38:06 -0600

    class progress pushed

--- 4_drf_actions_and_complex_api_design_start/vehiclefleet_frontend/src/components/StatCard.jsx
+++ 4_drf_actions_and_complex_api_design_start/vehiclefleet_frontend/src/components/StatCard.jsx
@@ -1,9 +1,10 @@
 export default function StatCard({label, value, color}) {
-  return  <div className={`card shadow-md`}>
+  return  <div className={`card shadow-md ${color}`}>
      <div className="card-body">
-        <p className="text-sm font-medium opacity-80">TEST</p>
+        <p className="text-sm font-medium opacity-80">{label}</p>
         <p className="text-4xl font-bold">
-          <span className="loading loading-spinner loading-sm" ></span>
+          {value}
+          {/* <span className="loading loading-spinner loading-sm" ></span> */}
         </p>
      </div>
     </div>
